b8d892d411bbfdb31cb6292cb5c981188a49ebd2
cboswel1 <christianboswell86@gmail.com>
2020-08-29 12:42:12 -0600

    sandbox folder

---
+++ sandbox/BulmaModalButton/dummyscript.js
@@ -0,0 +1,61 @@
+$("#launchModal").click(function() {
+ 
+    $(".modal").addClass("is-active"); 
+   
+  });
+   
+   
+   
+   
+  $(".modal-close").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
+   
+   
+   
+   
+  $("#closebtn").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
+   
+  $("#closecross").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
+   
+
+//BUtton 2 
+$("#launchZionModal").click(function() {
+ 
+    $(".modal").addClass("is-active"); 
+   
+  });
+   
+   
+   
+   
+  $(".modal-close").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
+   
+   
+   
+   
+  $("#closeZionbtn").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
+   
+  $("#closeZioncross").click(function() {
+   
+     $(".modal").removeClass("is-active");
+   
+  });
